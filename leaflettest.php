9e5b44178139ed9d68507ed33f873ba7777baf90
tbsouza <tbsouza@outlook.com>
2016-10-19 14:12:44 -0200

    Leaflet Mapa teste
    Criação de mapa interativo (Choropleth map) using Leaflet API for JavaScript

--- leaflettest.php
+++ leaflettest.php
@@ -15,6 +15,8 @@
 		<script src="leaflet.js" type="text/javascript"></script>
 		<!-- http://openlayers.org/en/v3.18.2/css/ol.css -->
 		<link rel="stylesheet" href="leaflet.css" type="text/css" >
+		<!-- http://openlayers.org/en/v3.18.2/build/ol.js -->
+		<script src="requirejs.js" type="text/javascript"></script>
 
 		<!-- JQuery -->
 		<script src="jquery-3.0.0.js" ></script>
@@ -33,7 +35,7 @@
 
   	<body>
 
-  	<!-- Campo que sera adicionado o mapa -->
+  		<!-- Campo que sera adicionado o mapa -->
   		<div id="map" class="map"></div>
 
   		<script type="text/javascript">
@@ -41,13 +43,31 @@
   			// cria um novo mapa
   			var map = L.map('map').setView([-15, -55], 5);
 
-  			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-			    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | CRR Inatel'
+  			L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+			    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | CRR '
 			}).addTo(map);
+
+  			//http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png
+  			//http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png
+  			//http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}
+  			//http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png
+  			//http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png
+  			//http://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}
+
+
+  			// Arquivo GeoJson com informações dos municípios
+  			var geojsonObject;
+
+  			// Abre o GeoJson com os dados
+  			$.getJSON("pop_2015_json.geojson", function(json) {
+				geojsonObject = L.geoJson(json, {style: style, onEachFeature: onEachFeature});
+				geojsonObject.addTo(map);
+			});
+
 
   			// Funcao para diferenciar o estilo de cada feicao
   			function getColor(p) {
-			    return p > 20000000 ? '#023858' :
+			    return p > 2000000  ? '#023858' :
 			           p > 600000   ? '#045a8d' :
 			           p > 300000   ? '#0570b0' :
 			           p > 90000    ? '#3690c0' :
@@ -56,7 +76,7 @@
 			                          '#d0d1e6';
 			}
 
-			// Funcao para aplicar o estilo
+			// Funcao para aplicar o estilo padrão
 			function style(feature) {
 			    return {
 			        fillColor: getColor(feature.properties.pop_2015),
@@ -68,42 +88,85 @@
 			    };
 			}
 
+			// Aplica o estilo ao passar o mouse
 			function highlightFeature(e) {
 			    var layer = e.target;
 
 			    layer.setStyle({
-			        weight: 5,
+			        weight: 3,
 			        color: '#666',
 			        dashArray: '',
-			        fillOpacity: 0.9
+			        fillOpacity: 0.7,
+			        opacity: 1
 			    });
 
 			    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
 			        layer.bringToFront();
 			    }
+
+			    info.update(layer.feature.properties);
 			}
 
+			// Limpa a formatação ao retirar o mouse
 			function resetHighlight(e) {
-			    geojson.resetStyle(e.target);
+			    geojsonObject.resetStyle(e.target);
+
+			    info.update();
 			}
 
+			// Da zoom para feição ao clicar
 			function zoomToFeature(e) {
 			    map.fitBounds(e.target.getBounds());
 			}
 
-			
-  			// Abre o GeoJson com os dados
-  			$.getJSON("pop_2015_json.geojson", function(json) {
+			// Aplica as funcionalidades a cada feição
+			function onEachFeature(feature, layer) {
+			    layer.on({
+			        mouseover: highlightFeature,
+			        mouseout: resetHighlight,
+			        click: zoomToFeature
+			    });
+			}
 
-				L.geoJson(json, {style: style}).addTo(map);
+			// Adiciona Campo para mensagem
+			var info = L.control();
 
-			    console.log(json); // this will show the info it in firebug console
-			});
+			info.onAdd = function (map) {
+			    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+			    this.update();
+			    return this._div;
+			};
+
+			// method that we will use to update the control based on feature properties passed
+			info.update = function (props) {
+			    this._div.innerHTML = '<h4>População por Município</h4>' +  (props ?
+			        '<b>' + props.nome + '</b><br />' + props.pop_2015 + ' habitantes</sup>'
+			        : ' ');
+			};
+
+			info.addTo(map);
 
 
+			// Adiciona legenda
+			var legend = L.control({position: 'bottomright'});
 
+			legend.onAdd = function (map) {
 
+			    var div = L.DomUtil.create('div', 'info legend'),
+			        grades = [0, 10000, 20000, 90000, 300000, 600000, 2000000],
+			        labels = [];
 
+			    // loop through our density intervals and generate a label with a colored square for each interval
+			    for (var i = 0; i < grades.length; i++) {
+			        div.innerHTML +=
+			            '<i class="legenda" style="background:' + getColor(grades[i] + 1) + '">&nbsp; &nbsp; &nbsp;</i> ' +
+			            grades[i] + (grades[i + 1] ? ' &ndash; ' + grades[i + 1] + '<br>' : ' +');
+			    }
+
+			    return div;
+			};
+
+			legend.addTo(map);
 
 
 
@@ -111,7 +174,7 @@
   		</script>
 
 
-
+  		<br/><br/><br/>
 
   	</body>
 
