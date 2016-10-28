dc21b0a5febd232e862ac5e95cb609081c50ffc6
tbsouza <tbsouza@outlook.com>
2016-10-28 19:21:06 -0200

    Add files via upload
    Novos botões: FullScreen e checkbox
    Ainda falta: colocar ações nos checkbox para 'filtrar' as regiões

--- leaflettest.php
+++ leaflettest.php
@@ -4,7 +4,7 @@
   	<head>
 
   		<meta charset="UTF-8"> <!-- Formato de codificação dos caracteres -->
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="Content-Type" content="text/html/map; charset=utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
 		
@@ -29,6 +29,13 @@
 
 		<!-- JavaScript Toastr -->
 		<script src="toastr.js" type="text/javascript"></script>
+		<!-- CSS Toatr -->
+		<link rel="stylesheet" href="toastr.css" type="text/css" >
+
+		<!-- JavaScript FullScreen -->
+		<script src="leaflet.fullscreen.min.js" type="text/javascript"></script>
+		<!-- CSS FullScreen -->
+		<link rel="stylesheet" href="leaflet.fullscreen.css" type="text/css" >
 
   	</head>
 
@@ -41,33 +48,39 @@
 
   		<script type="text/javascript">
   		
-  			// Configura o toaster
-  			toastr.options = {
-				"closeButton": false,
-				"debug": false,
-				"positionClass": "toast-bottom-full-width",
-				"onclick": null,
-				"showDuration": "300",
-				"hideDuration": "3000",
-				"timeOut": "5000",
-				"extendedTimeOut": "1000",
-				"showEasing": "swing",
-				"hideEasing": "linear",
-				"showMethod": "fadeIn",
-				"hideMethod": "fadeOut",
-				"newestOnTop": true,
- 				"progressBar": true
-			}
+  			// configura o toastr (toast messages)
+  			configureToast();
 
+			function configureToast(){
+	  			toastr.options = {
+					"closeButton": false,
+					"debug": false,
+					"positionClass": "toast-top-right",
+					"onclick": null,
+					"showDuration": "1000",
+					"hideDuration": "2500",
+					"timeOut": "3000",
+					"extendedTimeOut": "1000",
+					"showEasing": "linear",
+					"hideEasing": "linear",
+					"showMethod": "fadeIn",
+					"hideMethod": "fadeOut",
+					"newestOnTop": true,
+	 				"progressBar": true,
+	 				"escapeHtml": true
+				}
+			}
+			
 
   			// cria um novo mapa
-  			var map = L.map('map').setView([-15, -55], 5);
+  			var map = L.map('map', {fullscreenControl: true }).setView([-15, -55], 5);
 
   			// Seleciona o basemap
   			L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
 			    attribution: '&copy; <a target="_blank" href="http://www.inatel.br/crr/">CRR</a> Inatel',
-			    minZoom: 3, maxZoom: 13
+			    minZoom: 3, maxZoom: 13, unloadInvisibleTiles: true, updateWhenIdle: true, reuseTiles: true
 			}).addTo(map);
+
 
   			// Tipos de BaseMaps
   			//http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png
@@ -181,9 +194,12 @@
 			    // loop through our population intervals and generate a label with a colored square for each interval
 			    for (var i = 0; i < grades.length; i++) {
 			        div.innerHTML +=
-			            '<i class="legenda" style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+			            '<i class="legenda" style="background:' + getColor(grades[i] + 1) + '"></i><input id="check' + i + 
+			            '" checked=true type="checkbox"/> ' +
 			            grades[i] + (grades[i + 1] ? ' &ndash; ' + grades[i + 1] + '<br>' : ' +');
 			    }
+
+			    //</i><input id="check' + i + '" checked=true type="checkbox"/>
 
 			    return div;
 			};
@@ -203,11 +219,11 @@
 				// Botão para centralizar
 				L.easyButton('<strong>&Uparrow;</strong>', function(btn, map){
 				    map.setView([lat, lon], zoom);
-				}, 'Centro'),
+				}, 'Centralizar'),
 
 				// Botão para localizar posição do usuário
 				L.easyButton('<strong>&xodot;</strong>', function(btn, map){
-				    var location = map.locate({setView : true, maxZoom: 10});
+				    map.locate({setView : true, maxZoom: 10});
 				}, 'Localizar'),
 
 				// Botão para limpar marcadores
@@ -231,13 +247,20 @@
 				    fillOpacity: 0.4,
 				    radius: 650
 				}).addTo(map);
+
+				toastr.success("Localização encontrada");
 			}
 
 			map.on('locationfound', onLocationFound);
 
 			// Não encontrar localização
 			function onLocationError(e) {
-			    alert(e.message + "Não foi possível encontrar sua localização");
+
+				if( e.message != "Geolocation error: Position acquisition timed out." ){
+					toastr.error("Não foi possível encontrar sua posição");
+				}
+
+			    console.log(e.message);
 			}
 
 			map.on('locationerror', onLocationError);
@@ -245,6 +268,11 @@
 
 			// Funão para limpar os marcadores
 			function removeMarkers(){
+				
+				if( circle != null || popup != null ){
+					toastr.success("Campos limpos");
+				}
+
 				if( circle != null ) {
 				    map.removeLayer(circle);
 				    circle=null;
@@ -254,17 +282,52 @@
  					map.removeLayer(popup);
 				    popup=null;
 				}
-
-				//toastr.success("Message will come here", "Title");
 			}
 
-			
+
+/*
+			// Adiciona checkbox separado da legenda
+			// create the checkbox control
+			var command = L.control({position: 'bottomleft'});
+
+			command.onAdd = function (map) {
+			    var div = L.DomUtil.create('div', 'checkbox');
+
+			    div.innerHTML = '<form><input id="check1" checked=true type="checkbox"/>0 - 10000</form>';
+			    div.innerHTML += '<form><input id="check2" checked=true type="checkbox"/>10000 - 20000</form>';
+			    div.innerHTML += '<form><input id="check3" checked=true type="checkbox"/>20000 - 90000</form>';
+			    div.innerHTML += '<form><input id="check4" checked=true type="checkbox"/>90000 - 300000</form>';
+			    div.innerHTML += '<form><input id="check5" checked=true type="checkbox"/>300000 - 600000</form>';
+			    div.innerHTML += '<form><input id="check6" checked=true type="checkbox"/>600000 - 2000000</form>';
+			    div.innerHTML += '<form><input id="check7" checked=true type="checkbox"/>2000000 +</form>';
+
+
+			    return div;
+			};
+
+			command.addTo(map);
+
+
+			// add the event handler
+			function check0Clicked() {
+
+
+			   alert("Clicked, checked = " + this.checked);
+			}
+
+			document.getElementById("check0").addEventListener("click", check0Clicked, true);
+
+*/
+
+
 
 
   		</script>
 
+
   		<br/><br/><br/>
 
   	</body>
+
 
 </html>
